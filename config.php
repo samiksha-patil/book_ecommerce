cb0b9a6b97e906c77c45200871abeb8e4b1c7ed9
EnigmaticEcho <sakshiuppoor@gmail.com>
2020-09-29 11:06:53 +0530

    Add config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+
+?>
