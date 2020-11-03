22a0d2aa86794f9a1fadd0577d6584fa91f1d139
samiksha-patil <samiksha16.patil@gmail.com>
2020-11-03 18:53:06 +0530

    seperated styles from checkout template

--- order/checkout.php
+++ order/checkout.php
@@ -7,34 +7,74 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 
 require_once "../connection.php";
-
+$id= $_SESSION["user_id"];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>Checkout</title>
+        
+<link rel="stylesheet" href="../static/css/main.css" />
+<link rel="stylesheet" href="../static/css/styles.css" />
+<link rel="stylesheet" href="../static/css/form.css" />
+<link rel="stylesheet" href="../static/css/cart.css" />
+<link rel="stylesheet" href="../static/css/checkout.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
    
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
 </head>
 <body>
-Order Summary
+
+<div class="container-top">
+  <img
+    class="img-top"
+    src="../static/images/title-img.jpg"
+    alt="Cinque Terre"
+    width="1000"
+    height="300"
+  />
+  <div class="center product-title">
+    Checkout
+    <div class="elementor-divider-separator"></div>
+  </div>
+</div>
+
+
+                   
+
+
+
+
+<div class="row-checkout">
+
+
+
+
+
+
+
+
+<div style="padding: 2%;" class="column-50 ">
+    
+    <div style="text-align: center;" class="head-sub">Order Summary</div>
+   <table style="width:100%; max-width: 800px;">
+         
+   <tbody>
+   
+        
 <?php
 
-$id= $_SESSION["user_id"];
+
 $sql5 = "SELECT * FROM book NATURAL JOIN book_for_sale INNER JOIN cart_item on cart_item.book_id= book_for_sale.book_id WHERE cart_item.user_id=$id AND cart_item.is_ordered=0";
                     
                     if($result5 = mysqli_query($link, $sql5)){
                         if(mysqli_num_rows($result5) > 0){ ?>
-                            <div class="row books">
+                            
                             <?php 
                             $total =0;
                             while($row1 = mysqli_fetch_array($result5)){ ?>
-                                        <div class="col-sm-3">
+                                                    <tr>
                                           
                                                     <?php
                                                     $book_id =$row1["book_id"];
@@ -46,25 +86,140 @@ $sql5 = "SELECT * FROM book NATURAL JOIN book_for_sale INNER JOIN cart_item on c
                                                             
                                                         
                                                     ?>
-                                                    <div class="card-body">
-                                                    <h5 class="card-title"><?php echo $row1["title"]; ?></h5>
-                                                    <p class="card-text">Rs. <?php echo $row1["price"] ?></p>
+                                                    <td>  <img style="height: 65px; width: auto;" class="img-book" src="../uploads/<?php echo $row1["cover_image"]; ?>" alt="" /></td>
+                                                   <td> <?php echo $row1["title"]; ?></td>
+                                                    <td>$ <?php echo $row1["price"] ?></td></tr>
                                                      <?php   $total = $total+ $row1["price"];
                                                         }
 
                                                     }
                                                     ?>
-                                                </div>
-                                            </div>
-                                        </div>
+                                             
                                     <?php }                             
                             
                                 ?>
-                        </div>
+                      </tbody>
+    </table>
+    <div style="float: right; font-size: 27px;" class="head-sub">Total: $<?php echo "$total" ?></div>
+</div>
+
+
+
+<?php
+                    if($_SERVER["REQUEST_METHOD"] == "POST"){
+                        $id=$_SESSION["user_id"];;
+                        $address = $_POST['address'];
+                        $zip = $_POST['zip'];
+                        $state =$_POST['state'];
+                        $mode = $_POST['mode'];
                         
-                               
-                                    <h3 class="card-title"> Total:  <?php echo "$total" ?></h3>
-                                    <hr> 
+                        $sql = "INSERT INTO order_item (user_id,street,zipcode,state) VALUES ('$id','$address','$zip', '$state')";
+                        
+                        if(mysqli_query($link, $sql))
+                        {                                 
+                                       
+                            $sql1 = "INSERT INTO payment(order_id,payment_date,payment_amount,mode_of_payment) VALUES (LAST_INSERT_ID(),NOW(),'$total','$mode')";
+                            if(mysqli_query($link, $sql1))
+                            {
+                                
+                                $sql3= "UPDATE cart_item SET cart_item.is_ordered=1, cart_item.order_id=LAST_INSERT_ID()  WHERE cart_item.user_id=$id AND cart_item.is_ordered=0 AND cart_item.book_id  IN ( SELECT book_id FROM book_for_sale) AND cart_item.book_id NOT IN ( SELECT book_id FROM cart_item WHERE is_ordered=1)";
+                                
+                                if(mysqli_query($link, $sql3))
+                                {
+                                echo "Payment Successful!..you will soon receive your book.";
+                                }
+                            
+                            
+                            }
+                            } 
+                            
+                         
+                        
+                        else{
+                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                        }
+                         
+                        // Close connection
+                     
+                        
+                        }
+                        ?>
+                        
+
+
+
+
+
+
+
+
+
+
+
+    <div style="padding: 2%;" class="column-50">
+      <div  class="head-sub">Shipping Details: </div>
+        
+
+
+
+
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+<div class="login-page-new__main-form-row">
+    <label class="login-page-new__main-form-row-label">Address:</label>
+    <input  class="cu-form__input bg" type="text" name="address" required>
+                    </div>
+   <div class="login-page-new__main-form-row">
+    <label class="login-page-new__main-form-row-label">Postcode / Zip:</label>
+    <input type="text" name="zip" class="cu-form__input bg" required>
+     </div>
+<div class="login-page-new__main-form-row">
+    <label  class="login-page-new__main-form-row-label">State:</label>   
+    <input type="text" name="state"  class="cu-form__input bg" required>
+    </div>
+
+    <div style="font-size: 27px;" class="head-sub">Payment Method: </div>
+          <div id="payment">
+            <ul class=" payment_methods methods">
+            <li class=" payment_method_bacs">
+          <input id="payment_method_bacs" onclick="javascript:PaymentMethodCheck();" type="radio" class="input-radio" name="mode" value="Net Banking" checked="checked" data-order_button_text="">
+          
+          <label for="payment_method_bacs">
+          Direct bank transfer 	</label>
+            <div id="bank_transfer" class="payment_box payment_method_bacs" style="display: block;">
+            <p class="payment-under">Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p>
+          </div>
+          </li>
+          <li class="payment_method_cod">
+          <input id="payment_method_cod" onclick="javascript:PaymentMethodCheck();" type="radio" class="input-radio" name="mode" value="Cash on Delivery" data-order_button_text="">
+          
+          <label for="payment_method_cod">
+          Cash on delivery 	</label>
+            <div id="cod" class="payment_box payment_method_cod" style="display: none;">
+            <p class="payment-under">Pay with cash upon delivery.</p>
+          </div>
+          </li>
+          <li class="payment_method_paypal">
+          <input id="payment_method_paypal" onclick="javascript:PaymentMethodCheck();" type="radio" class="input-radio" name="mode" value="Credit Card" data-order_button_text="Proceed to PayPal">
+          
+          <label for="payment_method_paypal">
+          PayPal <img src="https://www.paypalobjects.com/webstatic/mktg/logo/AM_mc_vs_dc_ae.jpg" alt="PayPal acceptance mark">	</label>
+            <div id="paypal" class="payment_box payment_method_paypal" style="display: none;">
+            <p class="payment-under">Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.</p>
+          </div>
+          </li>
+          </ul>
+</div>          
+    
+<input class="place-order-btn" type="submit" name="submit" value="Proceed to pay">
+</form>
+</div>
+      
+
+</div>
+</div>
+                        
+                            
                                     
                                     <?php
                                
@@ -83,78 +238,35 @@ $sql5 = "SELECT * FROM book NATURAL JOIN book_for_sale INNER JOIN cart_item on c
                         echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
                     }
  
-                    // Close connection
+                    mysqli_close($link);
                    
                     ?>
-                    <?php
-                    if($_SERVER["REQUEST_METHOD"] == "POST"){
-                        $id=$_SESSION["user_id"];;
-                        $address = $_POST['address'];
-                        $zip = $_POST['zip'];
-                        $state =$_POST['state'];
-                        $mode = $_POST['mode'];
-                        
-                        $sql = "INSERT INTO order_item (user_id,street,zipcode,state) VALUES ('$id','$address','$zip', '$state')";
-                        
-                        if(mysqli_query($link, $sql))
-                        {                                 
-                                       
-                            $sql1 = "INSERT INTO payment(order_id,payment_date,payment_amount,mode_of_payment) VALUES (LAST_INSERT_ID(),NOW(),'$total','$mode')";
-                            if(mysqli_query($link, $sql1))
-                            {
-                                
-                                $sql3= "UPDATE cart_item SET cart_item.is_ordered=1, cart_item.order_id=LAST_INSERT_ID()  WHERE cart_item.user_id=$id AND cart_item.is_ordered=0 AND cart_item.book_id  IN ( SELECT book_id FROM book_for_sale) AND cart_item.book_id NOT IN ( SELECT book_id FROM cart_item WHERE is_ordered=1)";
-                                echo $sql3;
-                                if(mysqli_query($link, $sql3))
-                                {
-                                echo "Payment Successful!..you will soon receive your book.";
-                                }
-                            
-                            
-                            }
-                            } 
-                            
-                         
-                        
-                        else{
-                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-                        }
-                         
-                        // Close connection
-                        mysqli_close($link);
-                        
-                        }
-                        ?>
-                        
 
 
-
-
-
-
-
-
-
-
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+  <script>
+document.getElementById('bank_transfer').style.display = 'none';
+    document.getElementById('cod').style.display = 'none';
+    document.getElementById('paypal').style.display = 'none';
     
-    <label>Address:</label>
-    <input type="text" name="address" class="form-control" required>
-    <label>Postcode / Zip:</label>
-    <input type="text" name="zip" class="form-control" required>
-    <label>State:</label>
-    <input type="text" name="state" class="form-control"required>
+    
+            function PaymentMethodCheck() {
+                if (document.getElementById('payment_method_bacs').checked) {
+                  document.getElementById('bank_transfer').style.display = 'block';
+                  document.getElementById('cod').style.display = 'none';
+                  document.getElementById('paypal').style.display = 'none';
+                  
+                }
+                else if (document.getElementById('payment_method_cod').checked) {
+                  document.getElementById('bank_transfer').style.display = 'none';
+                  document.getElementById('cod').style.display = 'block';
+                  document.getElementById('paypal').style.display = 'none';
+                }
+                else if (document.getElementById('payment_method_paypal').checked) {
+                  document.getElementById('bank_transfer').style.display = 'none';
+                  document.getElementById('cod').style.display = 'none';
+                  document.getElementById('paypal').style.display = 'block';
+                }
+                
+            }
+</script>
 
-
-<h2>Payment method</h2>
-
-<label>Net Banking</label><input type="radio" name="mode" value="Net Banking" id="net_banking" required> 
-<label>Cash on Delivery</label><input type="radio" name="mode" value="Cash on Delivery" id="Cash on Delivery" required>
-<label>Credit Card</label><input type="radio" name="mode" value="Credit Card" id="Credit Card" required> 
-
-<input type="submit" name="submit" value="Proceed to pay">        
-</form>
-
-</body>
-</html>
