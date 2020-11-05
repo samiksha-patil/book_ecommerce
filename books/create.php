23838ffe9013539b38ada0faa946b81d43786209
sakshipatil16 <sakshi16.patil2000@gmail.com>
2020-11-05 19:14:14 +0530

    added styles to create.php

--- books/create.php
+++ books/create.php
@@ -113,40 +113,86 @@ mysqli_close($link);
 <title>Login</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="../static/css/form.css">
+<link rel="stylesheet" href="../static/css/styles.css">
+
+<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+<style>
+    .thumb-image{
+width:90%;
+ position:relative;
+ padding:5px;
+ margin: auto;
+}
+.hide_file {
+    position: absolute;
+    z-index: 1000;
+    opacity: 0;
+    cursor: pointer;
+    right: 0;
+    top: 0;
+    height: 100%;
+    font-size: 24px;
+    width: 100%;
+    
+}
+.abc{
+  padding:15px 15px;
+  background:#F7F7F7;
+  border:1px solid #ffaea0fc;
+  position:relative;
+ 
+  border-radius:2px;
+  text-align:center;
+  float:left;
+  cursor:pointer
+}
+</style>
 </head>
 
 <body>
- 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
     
 </head>
 <body>
-<div class="login-container">
-  <div class="login-row">
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+<div class="registration-container" style="max-width:1020px;">
+  <div class="login-row" >
+      <div style="margin:auto;">
+   <div >   
+    <div class="abc  login-page-new__main-form-row-label" for="cover_image">Cover Image <ion-icon name="cloud-upload-outline"></ion-icon>
 
-  <div style="padding: 30px 60px 26px;" >
-        <div class="login-page-new__main-form-title">Create a Book for sell</div>
+    <input  id="fileUpload" class="hide_file" type="file"  name="file">
+    </div>
+    </div>
+    <div style="margin:auto;"> 
+      <div  id="image-holder"> </div>
+</div></div>
+  <div class="">
+
+
+  <div style="padding: 30px 60px 0px 26px ;" >
+        <div class="login-page-new__main-form-title">Add a Book</div>
             <div class="login-page-new__main-form-row">
 
 
     <div class="<?php echo (!empty($title_err)) ? 'has-error' : ''; ?>">
-    <label class="login-page-new__main-form-row-label" >Title:</label>
+    <label class="login-page-new__main-form-row-label" >Title</label>
     <input class="cu-form__input" type="text" name="title" required>
     <span class="help-block"><?php echo $title_err; ?></span>
     </div>
     </div>
     <div class="login-page-new__main-form-row">
-    <label class="login-page-new__main-form-row-label">Description:</label>
+    <label class="login-page-new__main-form-row-label">Description</label>
     
     <input class="cu-form__input" type="text" name="info"  required>
     </div>
+    
+    <div style="padding:0px 0px;" class="row">
+    <div class="column-50 p-r">
     <div class="login-page-new__main-form-row">
-    <label class="login-page-new__main-form-row-label" for="cover_image">Cover Image"</label>
-    <input class="cu-form__input" type="file" name="file">
-    </div>
-    <div class="login-page-new__main-form-row">
-    <label class="login-page-new__main-form-row-label" for="category">Category"</label>
-    <select name="category">
+    <label class="login-page-new__main-form-row-label" for="category">Category</label>
+    <select style="margin-top:7px;" id="category" name="category">
     <option value="Textbook">Textbook</option>
     <option value="Memoir">Memoir</option>
     <option value="Essays">Essays</option>
@@ -155,32 +201,40 @@ mysqli_close($link);
     <option value="Comic Book or Graphic Novel">Comic Book or Graphic Novel</option>
     <option value="Fiction">Fiction</option>
     </select>
-    </div>
+    </div></div>
+    <div class="column-50 p-l">
     <div class="login-page-new__main-form-row">
-    <label class="login-page-new__main-form-row-label">Author:</label>
+    <label class="login-page-new__main-form-row-label">Author</label>
     <input class="cu-form__input" type="text" name="author" required>
     </div>
+    </div></div>
+    <div style="padding:0px 0px;" class="row">
+    <div class="column-50 p-r">
     <div class="login-page-new__main-form-row">
-    <label  class="login-page-new__main-form-row-label">Language:</label>
+    <label  class="login-page-new__main-form-row-label">Language</label>
     <input class="cu-form__input" type="text" name="language"  required>
     </div>
+    </div>
+    <div class="column-50 p-l">
     <div class="login-page-new__main-form-row">
-    <label class="login-page-new__main-form-row-label">No. of pages:</label>
+    <label class="login-page-new__main-form-row-label">No. of pages</label>
     <input  class="cu-form__input" type="number" name="pages"  required>
     </div>
-    <br><br>
-    
-    <div class="<?php echo (!empty($type_err)) ? 'has-error' : ''; ?>">
+</div>
+</div>
+<div style="padding:0px 0px;" class="row">  
+    <span class="<?php echo (!empty($type_err)) ? 'has-error' : ''; ?>">
     
     <input class="" type="radio" name="type" value="rent" id="rent" onclick="javascript:BooktypeCheck();" required> 
-    <label class="login-page-new__main-form-row-label">rent</label>
-    </div>
-
+    <label style="padding-right:20px;" >Rent</label>
+</span>
+    <span class="help-block"><?php echo $type_err; ?></span>
     <input class="" type="radio" name="type" value="sale" id="sale" onclick="javascript:BooktypeCheck();" required>
-    <label class="login-page-new__main-form-row-label">sale</label>
-   
-    <span class="help-block"><?php echo $type_err; ?></span><br>
+    <label >Sale</label>
+</span>
     
+</div>
+
 <div class=" <?php echo (!empty($price_err)) ? 'has-error' : ''; ?>">
 <span class="help-block"><?php echo $price_err; ?></span>
 <div id="for_sale">
@@ -198,8 +252,8 @@ mysqli_close($link);
     </div>
  </div>     
 </div>
-<input type="submit" name="submit" value="Upload">
-       
+
+<input class="login-page-new__main-form-button" type="submit" value="Upload">      
 </form>
 </div>
 </div>   
@@ -225,6 +279,39 @@ document.getElementById('for_rent').style.display = 'none';
             }
             
         }
+</script>
+<script>
+$(document).ready(function() {
+        $("#fileUpload").on('change', function() {
+          //Get count of selected files
+          var countFiles = $(this)[0].files.length;
+          var imgPath = $(this)[0].value;
+          var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+          var image_holder = $("#image-holder");
+          image_holder.empty();
+          if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+            if (typeof(FileReader) != "undefined") {
+              //loop for each file selected for uploaded.
+              for (var i = 0; i < countFiles; i++) 
+              {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                  $("<img />", {
+                    "src": e.target.result,
+                    "class": "thumb-image"
+                  }).appendTo(image_holder);
+                }
+                image_holder.show();
+                reader.readAsDataURL($(this)[0].files[i]);
+              }
+            } else {
+              alert("This browser does not support FileReader.");
+            }
+          } else {
+            alert("Pls select only images");
+          }
+        });
+      });
 </script>
 </body>
 </html>
